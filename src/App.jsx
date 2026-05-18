362770d23512bc2ba51f547b9d3cd4faabc97769
[dyad] <git@dyad.sh>
2026-05-18 10:45:10 +0530

    Reverted all changes back to version bb96ec6bdc313d510f3c39d03eb4d933dcc5c023

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LoadingScreen from './components/LoadingScreen';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import SelectedWorks from './components/SelectedWorks';
+import ClientProjects from './components/ClientProjects';
 import Skills from './components/Skills';
 import ProfessionalExperience from './components/ProfessionalExperience';
 import CreativeEngineering from './components/CreativeEngineering';
@@ -29,6 +30,7 @@ export default function App() {
           <main>
             <Hero />
             <SelectedWorks />
+            <ClientProjects />
             <Skills />
             <ProfessionalExperience />
             <CreativeEngineering />
